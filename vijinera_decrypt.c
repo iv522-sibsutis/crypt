c70f12a68b9a211b7dfae3bea47aa087a92dd2be
umorw <lusko777@gmail.com>
2016-05-14 05:53:45 +0300

    Add decrypt

---
+++ vijinera_decrypt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "crypt.h"
+
+
+char vijinera_decrypt(char key[], FILE *f1, FILE *f2)
+{
+	char c;
+    int cur = 0;
+    while (!feof(f1)){
+        if (fread(&c,1,1,f1)){
+            if ((c >= 'A') && (c <= 'z')){
+                if ( (c-key[cur]+'A') >= 'A') 
+                	c = c-key[cur]+'A'; 
+                else
+                    c = c-key[cur]+('z'+1);
+                	fprintf(f2,"%c",c);   
+                if (cur < strlen(key)-1) 
+                	++cur; 
+                else cur = 0;
+            }
+        }
+    }
+
+    fclose(f1);
+    fclose(f2);
+
+    return 0;
+}
