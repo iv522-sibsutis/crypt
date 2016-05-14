e76c5076ee56c9751f2e35723e40a27e7bc63bd3
Hamid <hkasimov1998@inbox.ru>
2016-05-14 20:13:26 +0600

    Change of main function

--- main.c
+++ main.c
@@ -30,12 +30,36 @@ int main()
     			scanf("%s",key);
     		vij(key, f1, f2);
     		break;
-    /*	case 2:
-    		break;
+    //	case 2:
+    //		break;
     	case 3:
+		printf("Шифровка или дешифровка?\n1 - шифровка\n2 - дешифровка\n");
+		int i;
+		scanf("%d", &i);
+		printf("Укажите имя файла с ключом: ");
+    		char file_key[100];
+    		scanf("%s", file_key);
+		f3 = fopen(file_key, "r+");
+   		if (f3 == NULL) {
+   			printf("Файл не найден.\n");
+   			exit(EXIT_FAILURE);
+   		}
+   		if (i == 1)
+    			crypt = vernam_crypt(text, f3);
+    		else if (i == 2)
+    			crypt = vernam_decrypt(text, f3);
+    		else {
+    			printf("Неправильный ввод.\n");
+    			exit(EXIT_FAILURE);
+    		}
+    		if (crypt == NULL) {
+    			printf("Ошибка. Длина ключа не соответствует длине текста.\n");
+    			exit(EXIT_FAILURE);
+    		}
+		printf("Обратите внимание, что при шифровании ключ может быть изменен для корректной работы алгоритма.\n");
     		break;
-   		case 4:
-    		break;
+   	//	case 4:
+    	//	break;
     */ default:
             printf( "Неправильный ввод.\n" );
     }
