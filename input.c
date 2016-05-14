0468250a85991507b0b359b89ca653965fda5637
Hamid <hkasimov1998@inbox.ru>
2016-05-14 20:14:47 +0600

    Add output function

--- input.c
+++ input.c
@@ -7,9 +7,19 @@ int input()
 	printf("\n Выберите алгоритм шифрования: \n");
 	printf(" 1 - алгоритм Вижинера\n ");
 	//printf(" 2 - алгоритм Цезаря\n");
-	//printf(" 3 - алгоритм Вернама\n");
+	printf(" 3 - алгоритм Вернама\n");
 	//printf(" 4 - алгоритм ???? \n");
 	printf(" Ваш выбор : \n\n");
 	scanf("%d", &inp);
 	return inp;
+}
+
+int output(char *text)
+{
+	FILE *stream = fopen("output.txt", "w");
+	if (stream == NULL)
+		return 0;
+	fprintf(stream, "%s", text);
+	fclose(stream);
+	return 0;
 }
