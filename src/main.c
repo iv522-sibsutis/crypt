5abebc8f4cb86f319dc6038b9e05bb405d81a331
Hamid <hkasimov1998@inbox.ru>
2016-05-21 23:23:27 +0600

    Change of main function

--- src/main.c
+++ src/main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "crypt.h"
 
 
 int main()
 {
-	int inp = 0;
-	FILE *f1;
-	FILE *f2;
-	FILE *f3;
-	char key[100];
-	char fname[100];
+	int i, inp = 0;
+	FILE *f1, *f2, *fkey;
+	char key[100], fname[100], file_key[100];;
 	char *crypt;
 
 	f2 = fopen("output.txt","w");
 	printf("Input file name: ");
 	scanf("%s", fname);
-    
+
 	f1 = fopen(fname,"r");
 	if (f1 == NULL) {
-        	printf("file not found\n");
-        	return 0;
+		printf("file not found\n");
+	 	return 0;
 	}
 
 	inp = input();
@@ -28,48 +26,42 @@ int main()
 	char *text = reading(f1);
 	switch (inp)
 	{
-    	case 1:
-    		printf("Ключ (слово до 99 букв): ");
-    			scanf("%s",key);
-    		vij(key, f1, f2);
-    		break;
-    //	case 2:
-    //		break;
-    	case 3:
-		printf("Шифровка или дешифровка?\n1 - шифровка\n2 - дешифровка\n");
-		int i;
-		scanf("%d", &i);
-		printf("Укажите имя файла с ключом: ");
-    		char file_key[100];
-    		scanf("%s", file_key);
-		f3 = fopen(file_key, "r+");
-   		if (f3 == NULL) {
-   			printf("Файл не найден.\n");
-   			exit(EXIT_FAILURE);
-   		}
-   		if (i == 1)
-    			crypt = vernam_crypt(text, f3);
-    		else if (i == 2)
-    			crypt = vernam_decrypt(text, f3);
-    		else {
-    			printf("Неправильный ввод.\n");
-    			exit(EXIT_FAILURE);
-    		}
-    		if (crypt == NULL) {
-    			printf("Ошибка. Длина ключа не соответствует длине текста.\n");
-    			exit(EXIT_FAILURE);
-    		}
-		printf("Обратите внимание, что при шифровании ключ может быть изменен для корректной работы алгоритма.\n");
-    		break;
-   	//	case 4:
-    	//	break;
+		case 1:
+			printf("Ключ (слово до 99 букв): ");
+			scanf("%s",key);
+			vij(key, f1, f2);
+			break;
+	//	case 2:
+	//		break;
+		case 3:
+			i = vernam_input(file_key);
+			fkey = fopen(file_key, "r+");
+			char *k = reading(fkey);
+			if (i == 1) {
+				k = realloc(k, strlen(text) * sizeof(char));
+				crypt = vernam_crypt(text, k);
+				output(k, fkey);
+			}
+			else if (i == 2)
+				crypt = vernam_decrypt(text, k);
+			else {
+				printf("Неправильный ввод.\n");
+				exit(EXIT_FAILURE);
+			}
+			if (crypt == NULL) {
+				printf("Ошибка. Длина ключа не соответствует длине текста.\n");
+				exit(EXIT_FAILURE);
+			}
+		break;
+	//	case 4:
+		//	break;
 		default:
 			printf( "Неправильный ввод.\n" );
 	}
-	output(crypt);
+	output(crypt, f2);
 	printf("Ваш результат находится в файле output.txt \n");
+	fclose(f1);
 	free(text);
-	return 0;	
+	return 0;
 }
-
 
