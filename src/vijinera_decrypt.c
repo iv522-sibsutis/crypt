e35a5ab4ef9caa057732e4b31503cf2534a26434
umorw <lusko777@gmail.com>
2016-05-24 22:50:11 +0300

    Change decrypt without reading from file

--- src/vijinera_decrypt.c
+++ src/vijinera_decrypt.c
@@ -3,27 +3,24 @@
 #include "crypt.h"
 
 
-char vijinera_decrypt(char key[], FILE *f1, FILE *f2)
+char* vijinera_decrypt(char key[], char* text)
 {
-	char c;
+	char* c = text;
     int cur = 0;
-    while (!feof(f1)){
-        if (fread(&c,1,1,f1)){
-            if ((c >= 'A') && (c <= 'z')){
-                if ( (c-key[cur]+'A') >= 'A') 
-                	c = c-key[cur]+'A'; 
-                else
-                    c = c-key[cur]+('z'+1);
-                	fprintf(f2,"%c",c);   
-                if (cur < strlen(key)-1) 
-                	++cur; 
-                else cur = 0;
-            }
-        }
-    }
-
-    fclose(f1);
-    fclose(f2);
-
-    return 0;
+    int i;
+	int l = strlen(c); 
+	
+	for(i=0; i<l; i++){
+		if ((c[i] >= 'A') && (c[i] <= 'z')){
+			if ( (c[i]-key[cur]+'A') >= 'A') 
+				c[i] = c[i]-key[cur]+'A'; 
+			else
+				c[i] = c[i]-key[cur]+('z'+1);
+			if (cur < strlen(key)-1) 
+				++cur; 
+			else cur = 0;  
+			}
+	}
+ 
+	return c;
 }
