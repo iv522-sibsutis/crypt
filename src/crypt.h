af411e85999e84ebd1a3fc8f31c68254d9f1d876
Hamid <hkasimov1998@inbox.ru>
2016-06-03 20:00:43 +0600

    Ads declaration of encryption and decryption functions with algorithm Caesar

--- src/crypt.h
+++ src/crypt.h
@@ -10,5 +10,7 @@ char* vijinera_encrypt(char key[], char* text);
 char* vijinera_decrypt(char key[], char* text);
 char *vernam_crypt(char *text, char *key);
 char *vernam_decrypt(char *crypt, char *key);
+char *cezar_crypt(int rotatorN, char *string4);
+char *cezar_decrypt(int rotatorN, char *string4);
 
  #endif
